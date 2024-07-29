c2cb0789094d7b0649ffeaae8a227203b64b726d
ibr <ibr.seckin@gmail.com>
2024-07-29 16:51:20 +0300

    morgan fix

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,50 +1,31 @@
 
+// const morgan = require('morgan');
+
+
+// const fs = require('node:fs');
+// const now = new Date();
+
+// const today = now.toISOString().split("T")[0];
+// module.exports  = morgan('combined', {
+// stream:fs.createWriteStream(`./logs/${today}.log`,{flags:'a+'})
+// })
+
+
+
 const morgan = require('morgan');
-
-
 const fs = require('node:fs');
+const path = require('node:path');
 const now = new Date();
 
 const today = now.toISOString().split("T")[0];
-module.exports  = morgan('combined', {
-stream:fs.createWriteStream(`./logs/${today}.log`,{flags:'a+'})
-})
+const logDirectory = path.join(__dirname, 'logs');
 
+// Log dosyasının kaydedileceği dizinin varlığını kontrol edin
+if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory, { recursive: true });
+}
 
-// "use strict";
-// /* -------------------------------------------------------
-//     EXPRESS - Personnel API
-// ------------------------------------------------------- */
-// // LOGGER
-// // npm i morgan
-// // https://expressjs.com/en/resources/middleware/morgan.html
+const logStream = fs.createWriteStream(path.join(logDirectory, `${today}.log`), { flags: 'a+' });
 
-// const morgan = require('morgan')
+module.exports = morgan('combined', { stream: logStream });
 
-// // app.use(morgan('tiny'))
-// // app.use(morgan('short'))
-// // app.use(morgan('dev'))
-// // app.use(morgan('common'))
-// // app.use(morgan('combined'))
-// // Custom Log:
-// // app.use(morgan('TIME=":date[iso]" - URL=":url" - Method=":method" - IP=":remote-addr" - Ref=":referrer" - Status=":status" - Sign=":user-agent" (:response-time[digits] ms)'))
-
-// // Write to File:
-// // const fs = require('node:fs')
-// // app.use(morgan('combined', {
-// //     stream: fs.createWriteStream('./access.log', { flags: 'a+' })
-// // }))
-
-// // Write to File - Day by day:
-// const fs = require('node:fs')
-// const now = new Date()
-// // console.log(now, typeof now)
-// const today = now.toISOString().split('T')[0]
-// // console.log(today, typeof today)
-// // app.use(morgan('combined', {
-// //     stream: fs.createWriteStream(`./logs/${today}.log`, { flags: 'a+' })
-// // }))
-
-// module.exports = morgan('combined', {
-//     stream: fs.createWriteStream(`./logs/${today}.log`, { flags: 'a+' })
-// })
